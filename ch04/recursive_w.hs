56d6e4efe0de877dc212ca6fb15f13cc46c2f44a
Wiggert <wloonstra@ebay.com>
2013-02-08 08:27:59 +0100

    haskell file for chapter 4

---
+++ ch04/recursive_w.hs
@@ -0,0 +1,15 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n-1) x
+  
+take' :: (Num i, Ord i) => i -> [a] -> a
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' (x:xs) = x : take (n-1) xs
